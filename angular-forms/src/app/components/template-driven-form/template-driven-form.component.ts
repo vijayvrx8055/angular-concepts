26b2478d6a160d49d18da74955bbb8298a91941b
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2023-12-24 13:59:49 +0530

    Fetching Data from Form

--- angular-forms/src/app/components/template-driven-form/template-driven-form.component.ts
+++ angular-forms/src/app/components/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { User } from '../../model/User';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -8,12 +9,14 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateDrivenFormComponent {
 submitted=false;
+user:User={firstName:'',lastName:'',email:'',gender:''};
   formSubmitted(userForm: NgForm){
     this.submitted=true;
-    console.log(userForm);
+    this.user.firstName=userForm.form.controls['firstName'].value;
+    this.user.lastName=userForm.form.controls['lastName'].value;
+    this.user.email=userForm.form.controls['email'].value;
+    this.user.gender=userForm.form.controls['gender'].value;
+    console.log('User:',this.user);
   }
 
-  clear(){
-    this.submitted=false;
-  }
 }
