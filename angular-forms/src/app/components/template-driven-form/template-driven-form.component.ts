c65fa05da2774616797ac5dfbe64f0e01e6a1189
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2023-12-24 14:19:50 +0530

    Adding @ViewChild

--- angular-forms/src/app/components/template-driven-form/template-driven-form.component.ts
+++ angular-forms/src/app/components/template-driven-form/template-driven-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from '../../model/User';
 
@@ -7,9 +7,24 @@ import { User } from '../../model/User';
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
-export class TemplateDrivenFormComponent {
+export class TemplateDrivenFormComponent implements OnInit{
+
 submitted=false;
+
+@ViewChild('userForm') userForm : NgForm | undefined;
 user:User={firstName:'',lastName:'',email:'',gender:''};
+
+  ngOnInit(): void {
+   setTimeout(()=>{
+    this.user.firstName="Firstname";
+    this.user.lastName="Lastname";
+    this.user.email="Sample@domain.com";
+    this.user.gender="male";
+    this.userForm?.setValue(this.user);
+   }); 
+  }
+
+
   formSubmitted(userForm: NgForm){
     this.submitted=true;
     this.user.firstName=userForm.form.controls['firstName'].value;
